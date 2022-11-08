af4ba7ca149ac53737186b5fc0d2e187e74bdaec
Sam Sweeney <samsweeneyy@gmail.com>
2022-11-08 19:26:58 +1100

    Added fade in animation for form

--- script.js
+++ script.js
@@ -7,5 +7,17 @@ const getQuote = () => {
       h1.innerText = randomQuote.text;
     });
 };
+/*
+    Fade in animation for the form
+*/
+const formObserver = new IntersectionObserver(entries=>{
+    entries.forEach(entry=>{
+        entry.target.classList.toggle("in-view", entry.isIntersecting);
+    })
+}, {
+    threshold: 0.5,
+})
+const formInput = document.querySelector("form");
+formObserver.observe(formInput);
 
 getQuote();
